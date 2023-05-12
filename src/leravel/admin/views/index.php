4adce0e589093065da675168152184b0c5adcca4
lera2od <119118688+lera2od@users.noreply.github.com>
2023-05-12 17:02:59 +0300

    Motd And News support

--- src/leravel/admin/views/index.php
+++ src/leravel/admin/views/index.php
@@ -1,7 +1,22 @@
-<?php 
+<?php
 
 $leravelInfo = json_decode(file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/leravel/leravel.json"), true);
 $leravelVersion = $leravelInfo["version"];
+
+if (!isset($leravelInfo["lastMotdCheck"]) || $leravelInfo["lastMotdCheck"] < time() - 86400) {
+    $leravelInfo["lastMotdCheck"] = time();
+
+    $motd = file_get_contents("https://raw.githubusercontent.com/lera2od/leravel/master/data/motd.txt");
+    $news = json_decode(file_get_contents("https://raw.githubusercontent.com/lera2od/leravel/master/data/news.json"), true);
+
+    $leravelInfo["motd"] = $motd;
+    $leravelInfo["news"] = json_encode($news);
+
+    file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/leravel/leravel.json", json_encode($leravelInfo));
+} else {
+    $news = json_decode($leravelInfo["news"], true);
+    $motd = $leravelInfo["motd"];
+}
 
 ?>
 <!DOCTYPE html>
@@ -20,11 +35,28 @@ $leravelVersion = $leravelInfo["version"];
     <?php include "include/sidebar.php" ?>
     <div class="content">
         <h1>Welcome, <?= $_SESSION["username"] ?>!</h1>
-        <?php if(isset($leravelInfo["latestVersion"]) && $leravelInfo["latestVersion"] > $leravelVersion){ ?>
+        <?php if (isset($leravelInfo["latestVersion"]) && $leravelInfo["latestVersion"] > $leravelVersion) { ?>
             <div class="newVersionReminderBanner">
                 <p>New version of Leravel is available. <a href="/?admin&route=update" class="btn">Update now</a></p>
             </div>
         <?php } ?>
+        <div class="news-and-motd">
+            <div class="news">
+                <h2>Message Of The Day!</h2>
+                <p style="margin-left: 10px;"><?= $motd ?></p>
+            </div>
+            <div class="news">
+                <h2>News</h2>
+                <div class="news-scroll">
+                    <?php foreach ($news as $new) : ?>
+                        <div class="new">
+                            <h3><?= $new["title"] ?></h3>
+                            <p><?= $new["content"] ?></p>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
